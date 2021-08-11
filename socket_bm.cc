ddaff425d4c238acd8d95206625c96213c5bb086
Nikhil Marathe <nsm.nikhil@gmail.com>
2021-08-10 22:23:11 -0700

    Initial commit which links to benchmark lib

---
+++ socket_bm.cc
@@ -0,0 +1,10 @@
+#include <benchmark/benchmark.h>
+
+static void BM_StringCopy(benchmark::State& state) {
+  std::string x = "hello";
+  for (auto _ : state)
+    std::string copy(x);
+}
+BENCHMARK(BM_StringCopy);
+
+BENCHMARK_MAIN();
